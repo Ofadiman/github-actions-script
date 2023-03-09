18263bb54b4d9c112bf8d52683c2acf4cee7581f
ofadiman <ofadiman@gmail.com>
2023-03-09 09:02:16 +0100

    refactor: pass flags to golang script

--- scripts/main.go
+++ scripts/main.go
@@ -1,35 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Environ())
-
 	fmt.Println("golang script executed")
 
-	input := os.Getenv("INPUT_MESSAGE")
-	fmt.Printf("selected file: %v\n", input)
+	message := flag.String("message", "default value", "description")
+	flag.Parse()
+	fmt.Printf("parsed cli flag: %v\n", *message)
 
 	githubOutput := os.Getenv("GITHUB_OUTPUT")
 
 	bytes, _ := os.ReadFile(githubOutput)
+	fmt.Println("\"\"\"")
 	fmt.Println(string(bytes))
+	fmt.Println("\"\"\"")
 
-	file, err := os.OpenFile(githubOutput, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(githubOutput, os.O_RDWR, 0644)
 	if err != nil {
 		panic(err)
 	}
 
-	if _, err := file.WriteString("value=foo\n"); err != nil {
+	if _, err = file.WriteString("value=foo\n"); err != nil {
 		log.Println(err)
 	}
 
 	bytes, _ = os.ReadFile(githubOutput)
+	fmt.Println("\"\"\"")
 	fmt.Println(string(bytes))
+	fmt.Println("\"\"\"")
 
 	err = file.Close()
 	if err != nil {
