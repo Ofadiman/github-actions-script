a3babba1f0e313ebf62d80c024fef2ade35ed39e
ofadiman <ofadiman@gmail.com>
2023-03-09 22:56:24 +0100

    refactor: unify scripts logic, unify actions logic, remove foo argument

--- .github/scripts/deno.ts
+++ .github/scripts/deno.ts
@@ -1,12 +1,9 @@
 import {parse} from 'https://deno.land/std@0.178.0/flags/mod.ts'
 
 const parsedArgs = parse(Deno.args, {
-  string: ['message', 'foo'],
+  string: ['message'],
 })
-
 console.log(`parsed message flag: ${parsedArgs.message}`)
-console.log(`parsed foo flag: ${parsedArgs.foo}`)
 
 const githubOutput = Deno.env.get('GITHUB_OUTPUT') as string
-
-await Deno.writeFile(githubOutput, new TextEncoder().encode(`value=example output\n`), {append: true})
+await Deno.writeFile(githubOutput, new TextEncoder().encode(`value=example\n`), {append: true})
